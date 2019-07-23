7a2f635df334066b308ab54c5b3bf2a016b15b03
GH-Edifire <50841744+GH-Edifire@users.noreply.github.com>
2019-07-22 19:45:48 -0700

    update LC 1

--- LeetCode/Problem 1.py
+++ LeetCode/Problem 1.py
@@ -12,31 +12,25 @@
 #------------------------------------------------------------------
 class Solution(object):
     def twoSum(self, nums, target):
-        index = 0
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         data = {}
-        # place list into dictionary
-        for i in nums:
-            data[index] = nums[index]
-            #print("data["+str(index)+"] = " + str(data.get(index)))
-            index = index + 1
-        # check if complement is in dictionary
-        for i in data:
-            check = target - data[i]
-            if(check in data.values()):
-                # found complementary values in dictionary
-                x = -1
-                y = -1
-                # return the first instances of complementary values
-                for j in data:
-                    if(data[j] == data[i]):
-                        x = j
-                    elif(data[j] == check):
-                        y = j
-                # build return indices
-                answer = [x, y]
-                return answer
-        # no complementary values
+        index = 0
+        for num in nums:
+            comp = target - nums[index]
+            if(comp in data.keys()):
+                return [data[comp], index]
+            data[num] = index
+            index += 1
         return 0
+    
+sol = Solution()
+example = [3,2,4]
+target = 6
+print(sol.twoSum(example,target))
     
 nums = [2, 7, 11, 15]
 target = 22

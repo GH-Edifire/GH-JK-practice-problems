46480ce526937a0263424bc6504d0748cf9cf2e6
GH-Edifire <50841744+GH-Edifire@users.noreply.github.com>
2019-07-23 20:38:34 -0700

    added alternative

--- LeetCode/Problem 21.py
+++ LeetCode/Problem 21.py
@@ -46,6 +46,25 @@ class Solution(object):
                 l2 = l2.next
                 pointer = pointer.next
         return newHead
+    
+    # alternative
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
         
     def printListNodes(self, head):
         while(head is not None):

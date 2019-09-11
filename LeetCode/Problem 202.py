8122edf33df83f4cf1d1c6751f1f00b627e546e1
GH-Edifire <50841744+GH-Edifire@users.noreply.github.com>
2019-09-11 10:26:02 -0700

    LC 202

---
+++ LeetCode/Problem 202.py
@@ -0,0 +1,43 @@
+# LeetCode: Problem 202
+# Jonathan Kosasih
+
+#Write an algorithm to determine if a number is "happy".
+#
+#A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+#
+#Example: 
+#Input: 19
+#Output: true
+#Explanation: 
+#12 + 92 = 82
+#82 + 22 = 68
+#62 + 82 = 100
+#12 + 02 + 02 = 1
+#------------------------------------------------------------------
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if(not n):
+            return False
+        memo = {"0": 0,"1": 1, "2": 4, "3": 9, "4": 16, "5": 25, "6": 36, "7": 49, "8": 64,"9": 81}
+        seen = set()
+        string = str(n)
+        sum = 0
+        while(string != "1"):
+            if(string in seen):
+                return False
+            else:
+                seen.add(string)
+            for char in string:
+                sum += memo[char]
+            string = str(sum)
+            sum = 0
+        return True
+        
+        
+sol = Solution()
+example = 22
+print(str(sol.isHappy(example)))

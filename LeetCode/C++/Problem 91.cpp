34dac12d6812be0da842222e49391ca025808bb2
GH-Edifire <50841744+GH-Edifire@users.noreply.github.com>
2019-12-18 13:20:43 -0800

    update

--- LeetCode/C++/Problem 91.cpp
+++ LeetCode/C++/Problem 91.cpp
@@ -27,7 +27,19 @@ using std::set;
 class Solution {
 public:
 	int numDecodings(string s) {
-
+		if (s.empty()) return 0;
+		if (s.size() == 1 && stoi(s) != 0) return 1;
+		int dn_minus_2 = s[0] != '0' ? 1 : 0; // [1:9] are valid one-digit encoding
+		int dn_minus_1 = s[1] != '0' ? dn_minus_2 : 0;
+		int dn = 0;
+		for (int i = 2; i <= s.size(); ++i) {
+			int one = stoi(s.substr(i - 1, 1)), two = stoi(s.substr(i - 2, 2));
+			dn = (one != 0) ? dn_minus_1 : 0;
+			dn += (10 <= two && two <= 26) ? dn_minus_2 : 0; // [10:26] are valid two-digit encoding
+			dn_minus_2 = dn_minus_1;
+			dn_minus_1 = dn;
+		}
+		return dn;
 	}
 };
 

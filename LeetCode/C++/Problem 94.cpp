9d00fcc7bede2114d945fab5cfbc3839c5933395
GH-Edifire <50841744+GH-Edifire@users.noreply.github.com>
2020-01-06 19:22:18 -0800

    LC 94

---
+++ LeetCode/C++/Problem 94.cpp
@@ -0,0 +1,75 @@
+// LeetCode: Problem 94
+// Jonathan Kosasih
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+*/
+//-------------------------------------------------------------------
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <algorithm> 
+#include <stack>
+using std::vector;
+using std::map;
+using std::unordered_map;
+using std::set;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> inorderTraversal(TreeNode* root) {
+		vector<int> answer;
+		if (root == NULL) {
+			return answer;
+		}
+		this->inorderHelper(root, answer);
+		return answer;
+	}
+
+	void inorderHelper(TreeNode* root, vector<int>& answer) {
+		if (root->left) {
+			this->inorderHelper(root->left, answer);
+		}
+		answer.push_back(root->val);
+		if (root->right) {
+			this->inorderHelper(root->right, answer);
+		}
+	}
+
+	vector<int> inorderTraversalIter(TreeNode* root) {
+		vector<int> answer;
+		if (root == NULL) {
+			return answer;
+		}
+		vector<TreeNode*> stack;
+		while (!stack.empty() || root != NULL) {
+			if (root) {
+				stack.push_back(root);
+				root = root->left;
+			}
+			else {
+				TreeNode* temp = stack.pop_back();
+				answer.push_back(temp);
+				root = temp->right;
+			}
+		}
+		return answer;
+	}
+};
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+}

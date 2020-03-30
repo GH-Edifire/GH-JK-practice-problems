f1d8f63cee0c0d1545ebaddb4cfeac966d1ca8c4
GH-Edifire <50841744+GH-Edifire@users.noreply.github.com>
2020-03-30 10:59:31 -0700

    LC 237

---
+++ LeetCode/C++/Problem 237.cpp
@@ -0,0 +1,46 @@
+// LeetCode: Problem 237
+// Jonathan Kosasih
+/*
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+*/
+//-------------------------------------------------------------------
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <algorithm> 
+#include <stack>
+#include <queue>
+#include <deque>
+#include <numeric>
+using std::vector;
+using std::map;
+using std::unordered_map;
+using std::set;
+using std::stack;
+using std::queue;
+using std::deque;
+using std::accumulate;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	void deleteNode(ListNode* node) {
+		node->val = node->next->val;
+		node->next = node->next->next;
+	}
+};
+int main()
+{
+    //std::cout << "Hello World!\n";
+}

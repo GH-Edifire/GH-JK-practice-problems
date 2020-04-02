5b0bcf69a6b4f532e3bc37fe34a5197e6909825a
GH-Edifire <50841744+GH-Edifire@users.noreply.github.com>
2020-04-02 10:20:21 -0700

    LC 242

---
+++ LeetCode/C++/Problem 242.cpp
@@ -0,0 +1,46 @@
+// LeetCode: Problem 242
+// Jonathan Kosasih
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+*/
+//-------------------------------------------------------------------
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <algorithm> 
+#include <stack>
+#include <queue>
+#include <deque>
+#include <numeric>
+using std::vector;
+using std::map;
+using std::unordered_map;
+using std::set;
+using std::stack;
+using std::queue;
+using std::deque;
+using std::accumulate;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        int counts[26] = { 0 };
+        for (int i = 0; i < n; i++) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+            if (counts[i]) return false;
+        return true;
+    }
+};
+int main()
+{
+    //std::cout << "Hello World!\n";
+}
